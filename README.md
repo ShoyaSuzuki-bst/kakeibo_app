b856ec4648fdec9618946b6df060ddfab742a009
ShoyaSuzuki-bst <sh.suzuki@buysell-technologies.com>
2022-01-17 00:52:23 +0900

    [wip]ログイン機能

--- README.md
+++ README.md
@@ -1,16 +1,13 @@
 # kakeibo_app
+シンプルな家計簿アプリ
 
-A new Flutter project.
+1. このリポジトリをクローン
+1. パッケージインストール
+   - `flutter pub get`
+1. 必要な設定ファイルを設置
+`google-services.json`と`GoogleService-Info.plist`をfirebaseからダウンロード&設置
+ファイルを設置するパスは以下のように配置
+   - `ios/Runner/GoogleService-Info.plist`
+   - `android/app/google-services.json`
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+※この環境構築方法は実際に試したことがないので要検証
---
+++ lib/pages/my_home_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool _counter = false;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter = !_counter;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'テストテキスト Hello World!',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
